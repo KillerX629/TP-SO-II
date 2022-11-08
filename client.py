135576aae75383754e5bd768edf0c04f58de6975
Ignacio Pita <napita2007@gmail.com>
2022-11-08 12:11:17 -0300

    cambios en las funciones expuestas al cliente

--- client.py
+++ client.py
@@ -3,6 +3,9 @@ import rpyc
 #la conexión debe tener la dirección IP entre comillas
 conn = rpyc.connect("localhost", port=18861)
 
+#se usa conn.root para hacer las llamadas a servicios externos
+#root trae el objeto raíz del servidor (en este caso el servicio)
 
 print(conn.root.get_answer())
+#se usa stop para cerrar la conexión
 conn.root.stop()
