da50e3ac2e522029c949bfe5f2f32536b688cea8
Ignacio Pita <napita2007@gmail.com>
2022-11-08 12:47:35 -0300

    Cambios a las funciones del servidor

--- server.py
+++ server.py
@@ -5,27 +5,23 @@ from rpyc.utils.server import ThreadedServer  # or ForkingServer
 import sympy as sp
 
 
-
 class Calculadora(rpyc.Service):
     def on_connect(self, conn):
-        # code that runs when a connection is created
-        # (to init the service, if needed)
         pass
 
     def on_disconnect(self, conn):
-        # code that runs after the connection has already closed
-        # (to finalize the service, if needed)
-        pass
-
+        self.stop()
+        
     #usamos esta funcion para establecer las variables de ecuaciones
     def exposed_create_symbol(self,character):
-        if character not in self.symbols:
+        if sp.Symbol(character) not in self.symbols:
             self.symbols.append(sp.Symbol(character))
             return "Variable creada"
         else:
             return "Variable ya existe"
+    #este es un paso necesario, ya que el módulo sympy necesita saber cuales son las variables que se van a usar
     
-
+    #esta es la función expuesta al cliente para resolver ecuaciones
     def exposed_find_roots(self,eq):
         return self.solve_eq(eq)
     
@@ -35,14 +31,8 @@ class Calculadora(rpyc.Service):
     
     def exposed_get_answer(self):  # this is an exposed method
         return 42
-
-    exposed_the_real_answer_though = 43     # an exposed attribute
-
     symbols = []
     
-    def get_question(self):  # while this method is not exposed
-        return "what is the airspeed velocity of an unladen swallow?"
-
 
 if __name__ == "__main__":
     t = ThreadedServer(Calculadora, port=18861)
